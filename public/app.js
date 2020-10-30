4c55fc3c1e39a654e9f66b5387d5388d451d025f
Magpie <edchavez821@gmail.com>
2020-10-30 11:36:40 -0600

    Responsive card size

--- public/app.js
+++ public/app.js
@@ -72,7 +72,7 @@ $(document).on("click", "img", function() {
       // An input to enter a new title
       $("#notes").append("<input id='titleinput' name='title' >");
 
-      $("#titleinput").val(data[0].artist + "-" + data[0].title);
+      $("#titleinput").val(data[0].artist + " - " + data[0].title);
       // A textarea to add a new note body
       $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
       // A button to submit a new note, with the id of the article saved to it
